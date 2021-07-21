1f9a6530ef56a79ae9d656edba03c2a6d7756284
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-07-21 15:13:36 -0600

    generalized axios url

--- app/Services/AxiosService.js
+++ app/Services/AxiosService.js
@@ -1,9 +1,9 @@
 export const monApi = axios.create({
-  baseURL: 'https://pokeapi.co/api/v2/pokemon?limit=151',
+  baseURL: 'https://pokeapi.co/api/v2/',
   timeout: 5000
 })
 
 export const myMonApi = axios.create({
-  baseURL: 'https://bcw-sandbox.herokuapp.com/api/classroom/',
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/LucWeidell/',
   timeout: 5000
 })
