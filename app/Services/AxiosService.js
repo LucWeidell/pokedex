626e55419a560a81cc1376c413d22a4e6ec1444a
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-07-21 13:44:52 -0600

    added base apis

--- app/Services/AxiosService.js
+++ app/Services/AxiosService.js
@@ -1,4 +1,9 @@
 export const monApi = axios.create({
-  baseURL: '',
+  baseURL: 'https://pokeapi.co/api/v2/pokemon?limit=151',
+  timeout: 5000
+})
+
+export const myMonApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/classroom/',
   timeout: 5000
 })
